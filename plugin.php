be92a40ac80699c2876f2df2627e1d0e7aca3cb5
Theodor Truffer <tt@studer-raimann.ch>
2020-03-18 14:41:47 +0100

    pluginv ersion 1.3.0

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 $id = "xudf";
-$version = "1.2.3";
+$version = "1.3.0";
 $ilias_min_version = "5.4.0";
 $ilias_max_version = "5.4.999";
 $responsible = "studer + raimann ag";
